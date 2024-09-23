474afcd1e1a2b7c33162a7ab2ccdb9f739ce93b1
Koswara <koswara.c79@gmail.com>
2024-09-23 08:54:53 +0700

    first commit

---
+++ src/test/java/com/juaracoding/spring_storage/SpringStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.spring_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
